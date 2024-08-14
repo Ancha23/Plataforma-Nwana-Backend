015038cf7403fe4af21707a24dfa1562424977aa
jubisse <jubisse@hotmail.com>
2024-08-15 00:48:51 +0200

    uploads

--- src/config/uploadConfig.js
+++ src/config/uploadConfig.js
@@ -1,7 +1,5 @@
-const express = require('express');
-const router = express.Router();
 const multer = require('multer');
-const ClothingItem = require('./models/ClothingItem'); // Adjust path as needed
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -13,19 +11,3 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
-router.post('/clothing-items', upload.array('images', 6), async (req, res) => {
-    try {
-        const imageUrls = req.files.map(file => file.path);
-        const clothingItem = new ClothingItem({
-            ...req.body,
-            imageUrls
-        });
-        await clothingItem.save();
-        res.status(201).json(clothingItem);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-module.exports = router;
